6a1fc117cb18ed79135620bb88b5a0adb35b299a
jiangxiankang19941213 <13545635261@163.com>
2020-12-21 00:46:42 +0800

    调整eureka消费者

--- j-cloud-provider1/src/main/java/com/springcloud/jcloudprovider1/JCloudProvider1Application.java
+++ j-cloud-provider1/src/main/java/com/springcloud/jcloudprovider1/JCloudProvider1Application.java
@@ -4,8 +4,10 @@ import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class, MybatisAutoConfiguration.class})//不关联数据库时配置
+@EnableEurekaClient
 public class JCloudProvider1Application {
 
 	public static void main(String[] args) {
