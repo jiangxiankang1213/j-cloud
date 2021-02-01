9603a201b1f33ec8fd6fd74523d13e8e7b5f43ff
jiangxiankang19941213 <13545635261@163.com>
2021-02-01 16:40:42 +0800

    jxk提交企业监控3

---
+++ j-cloud-provider1/src/main/java/com/springcloud/jcloudprovider1/controller/qyjk3.java
@@ -0,0 +1,13 @@
+package com.springcloud.jcloudprovider1.controller;
+
+/**
+ * @USER: Jiang-XK
+ * @DESCRIPTION:
+ * @DATE: 2021/2/1 16:31
+ * @param:
+ */
+public class qyjk3 {
+    public static void main(String[] args) {
+        System.out.println("企业监控3");
+    }
+}
