0c5227ec9d9d337b06a396a9331e8cec1b09dd31
jiangxiankang19941213 <13545635261@163.com>
2021-02-18 09:05:55 +0800

    3333

--- j-cloud-provider1/src/main/java/com/springcloud/jcloudprovider1/controller/qyjk3.java
+++ j-cloud-provider1/src/main/java/com/springcloud/jcloudprovider1/controller/qyjk3.java
@@ -11,6 +11,6 @@ public class qyjk3 {
         System.out.println("企业监控3jxk3-3333");
 
         System.out.println("企业监控3jxk3-2222");
-
+        System.out.println("企业监控3jxk3-22223333333333");
     }
 }
